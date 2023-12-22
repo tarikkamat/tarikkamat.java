e2f166dddf9211a24b5441fe90af32349966900e
Tarık KAMAT <tarikkamat@Tarks-MacBook-Air.local>
2023-12-23 00:32:10 +0300

    add data types

---
+++ dataTypes/src/main/java/com/tarikkamat/Main.java
@@ -0,0 +1,33 @@
+package com.tarikkamat;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Data types
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+        byte myByte = 100;           // Byte
+        short myShort = 5000;        // Short
+        long myLong = 15000000000L;  // Long
+        float myFloat = 5.75f;       // Float
+        double myDouble = 19.99d;    // Double
+
+
+        // Print to console data types
+        System.out.println(myNum);
+        System.out.println(myFloatNum);
+        System.out.println(myLetter);
+        System.out.println(myBool);
+        System.out.println(myText);
+        System.out.println(myByte);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myFloat);
+        System.out.println(myDouble);
+    }
+}
