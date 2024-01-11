2e380147bd451ea6ee81023a74f8d7422228163a
Tarık KAMAT <tarikkamat@Tarks-MacBook-Air.local>
2024-01-12 02:43:13 +0300

    stocmanagment created

--- hotelBooking/src/main/java/com/tarikkamat/hotelbooking/model/BookedRoom.java
+++ hotelBooking/src/main/java/com/tarikkamat/hotelbooking/model/BookedRoom.java
@@ -1,4 +1,13 @@
 package com.tarikkamat.hotelbooking.model;
 
+import java.time.LocalDate;
+
 public class BookedRoom {
+    private Long bookingId;
+    private LocalDate checkInDate;
+    private LocalDate checkOutDate;
+    private String guestFullName;
+    private String guestEmail;
+    private int numberOfAdults;
+    private int numberOfChildren;
 }
